f121ec48e3f0cc74bf7f82e67ce9f9057689d0fe
bifshteks <artem.pif@mail.ru>
2017-01-16 17:46:18 +0300

    done

--- 19.py
+++ 19.py
@@ -1,55 +1,62 @@
-n = 3    #int(input())
-'''table = {}
-a = []
-for p in range(n):
-    game = input().split(';')
-
-    a.extend([{game[0]: int(game[1]), game[2]: int(game[3]) }])
-
-    if game[0] not in table:
-        table.update([(game[0], [int(game[1])])])
-    elif game[0] in table:
-        table[game[0]].append(int(game[1]))
-
-    if game[2] not in table:
-        table.update([(game[2], [int(game[3])])])
-    elif game[2] in table:
-        table[game[2]].append(int(game[3]))
-
-
 '''
-def dif_item(d, key):
-    lst = list(d.items())
-    print('lst =====', lst)
-    if lst[0][0] == d[key]:
-        return lst[1][1]
+Напишите программу, которая принимает на стандартный вход список
+игр футбольных команд с результатом матча и выводит на стандартный
+вывод сводную таблицу результатов всех матчей.
+
+За победу команде начисляется 3 очка, за поражение — 0, за ничью — 1.
+
+Формат ввода следующий:
+В первой строке указано целое число n
+— количество завершенных игр.
+После этого идет n
+
+строк, в которых записаны результаты игры в следующем формате:
+Первая_команда;Забито_первой_командой;Вторая_команда;Забито_второй_командой
+
+
+Вывод программы необходимо оформить следующим образом:
+Команда:Всего_игр Побед Ничьих Поражений Всего_очков
+
+Конкретный пример ввода-вывода приведён ниже.
+
+Порядок вывода команд произвольный.
+'''
+n = int(input())
+games = []
+
+for t in range(n):
+    x = input().split(';')
+    y = [tuple(x[:2]), tuple(x[2:])]
+    games.append(y)
+
+'''games = [[('Зенит', 3), ('Спартак', 1)],
+         [('Спартак', 1), ('ЦСКА', 1)],
+         [('ЦСКА', 0), ('Зенит', 2)]]'''
+comands = set()
+
+for lst in games:
+    for tpl in lst:
+        comands.add(tpl[0])
+gms = {k : [0, 0, 0, 0, 0] for k in comands}
+        
+for lst in games:
+    gms[lst[0][0]][0] += 1  # 1 games
+    gms[lst[1][0]][0] += 1  # 2 games
+    if int(lst[0][1]) > int(lst[1][1]):
+        gms[lst[0][0]][4] += 3 # 1 all point
+        gms[lst[0][0]][1] += 1 # 1 win 
+        gms[lst[1][0]][3] += 1 # 2 lose
+    elif int(lst[0][1]) < int(lst[1][1]):
+        gms[lst[1][0]][4] += 3# 2 all point
+        gms[lst[1][0]][1] += 1 # 2 win
+        gms[lst[0][0]][3] += 1 # 1 lose
     else:
-        return lst[0][1]
-#        table.add((game[0], int(game[1])),(game[2], int(game[3])))
-#inp = [ {a: 1, b: 2}, {c: 1, b: 3} , ...]
-table = {'Зенит': [3, 2], 'Спартак': [1, 1], 'ЦСКА': [1, 0]}
-a = [ {'Зенит': 3,'Спартак': 1}, {'Спартак': 1, 'ЦСКА': 1}, {'ЦСКА': 0,'Зенит': 2}]
-
-res = {}
-for com, nums in table.items():
-    win = 0
-    lose = 0
-    pat = 0
-    for d in a:
-        if com in d:
-            if d[com] > dif_item(d, com):
-                win += 1
-
-            elif d[com] < dif_item(d, com):
-                lose += 1
-                
-            else:
-                pat += 1
-        print('coma = ', com)
-        print('win =',win, 'lose =',lose, 'pat =',pat)
-    
-    list_com = [( com, [ len(nums), win, pat, lose,  win*3 + pat ])]
-    res.update(list_com)
-
-print(res)
-print( dif_item(a[0], 'Зенит'))
+        gms[lst[0][0]][4] += 1 # 1 all points
+        gms[lst[1][0]][4] += 1 # 2 all points
+        gms[lst[0][0]][2] += 1 
+        gms[lst[1][0]][2] += 1 
+for key in gms:
+    print(key, end='')
+    print(':', end='')
+    [print(num, end=' ') for num in gms[key][:-1]]
+    print(gms[key][-1])
