b12d6c39cda51d54c704943fe6d15b04eca6abcf
bifshteks <artem.pif@mail.ru>
2017-02-02 20:51:38 +0300

    done

--- points.py
+++ points.py
@@ -1,53 +1,47 @@
+'''
+По данным n
+
+отрезкам необходимо найти множество точек минимального размера,
+для которого каждый из отрезков содержит хотя бы одну из точек.
+
+В первой строке дано число 1≤n≤100
+отрезков. Каждая из последующих n строк содержит по два числа
+0≤l≤r≤109, задающих начало и конец отрезка. Выведите оптимальное
+число m точек и сами m точек. Если таких множеств точек несколько,
+выведите любое из них.
+'''
+
 n = int(input())
-a = []
+v = []
 points = []
 for i in range(n):
-    x = [int(k) for k in input().split()]
-a.append(list(x))
-a = sorted(a, key=lambda tpl: tpl[0])
-end = len(a)
-i = 1
-print('a = ', a)
-def foo(lst, i):
-    if lst[i][1] >= lst[i+1][0] and a[i-1][1] >= a[i+1][0]:
-        v = 1
-        while True:
-            if a[i-1][1] <= a[i+v][1]:
-                break
-            v += 1
-        for j in range(v):
-            pass
-    elif lst[i][1] >= lst[i+1][0]:
-        return [lst[i+1][0],lst[i][1]]
+    x, y = input().split()
+
+    v.append(tuple([int(x), int(y)]))
+dots = set()
+# сортируем по концам отрезков, потому что конец
+#считается в задаче "оптимальной" точкой и конец как раз и определяет
+#возможность пересечения
+v = sorted(v, key=lambda x: x[1])
+"""
+всегда берем эту точку, потому что в отсортированном по концу
+отрезка массиве всегда есть хотя бы один больше, либо отрезки
+не пересекаются вообще
+"""
+r = v[0][1]
+
+dots.add(r)
+for pair in v:
+    # если последняя точка не в отрезке
+    # если конец не находится в следующем отрезке
+    # если не пересекаются
+    if not (r >= pair[0] and r <= pair[1]):
+        r = pair[1] # надежная точка заменяется на конец следующего
+        dots.add(r)
+
+print (len(dots))
+print (' '.join([str(_) for _ in dots]))
+
+           
 
 
-while True:
-    if len(a) > 2:
-        if a[i-1][1] >= a[i][0] and a[i-1][1] >= a[i+1][0]: # if cover both
-            if a[i+1][0] > a[i][1]:
-                points.append(a[i][1])
-                del a[1], a[0]
-            else:
-                a[i][1] = a[i-1][1]
-                del a[0]
-        else:
-            points.append(a[i][0])
-            del a[0]
-        print('a = ', a)
-        print('points = ', points)
-    elif len(a) == 2:
-        if a[i-1][1] >= a[i][0]:
-            a[i][1] = a[i-1][1]
-            del a[0]
-
-        else:
-            points.append(a[i][0])
-            del a[0]
-        print('a = ', a)
-        print('points = ', points)
-    else:
-        points.append(set(a[0]).pop())
-        print('a[0] = ', a[0])
-        break
-print('dlina =',len(points))
-[print(' Tochka =',point, end=' ') for point in points]
